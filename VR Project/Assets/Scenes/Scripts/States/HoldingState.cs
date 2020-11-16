04565f5a18478399d0d541dfee4b3c6cb669f8a6
JustinM7 <jma7x3@gmail.com>
2020-11-15 22:12:51 -0800

    Base Version1.0
    TODO: State Machine

---
+++ VR Project/Assets/Scenes/Scripts/States/HoldingState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HoldingState : EmptyState
+{
+    private Hand_Animator_Script _hand;
+    public HoldingState(Hand_Animator_Script hand) : base(hand.gameObject)
+    {
+        _hand = hand;
+    }
+
+    public override Type Tick()
+    {
+        return typeof(EmptyState);
+    }
+
+
+}
---
+++ VR Project/Assets/Scenes/Scripts/States/StateMachine.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public  class StateMachine : MonoBehaviour
+{
+    private Dictionary<Type, EmptyState> _availiblestate;
+
+    public EmptyState CurrentState { get; private set; }
+    public event Action<EmptyState> OnStateChange;
+
+
+    public void SetState(Dictionary<Type, EmptyState> state)
+    {
+        _availiblestate = state;
+    }
+
+    private void Update()
+    {
+        if (CurrentState == null)
+        {
+            CurrentState = _availiblestate.Values.First();
+        }
+        var nextState = CurrentState?.Tick();
+
+        if (nextState != null && nextState != CurrentState?.GetType())
+        {
+            SwitchToNewState(nextState);
+        }
+    }
+
+
+    public void SwitchToNewState(Type nextState)
+    {
+        CurrentState = _availiblestate[nextState];
+        OnStateChange?.Invoke(CurrentState);
+    }
+    public virtual IEnumerator Start()
+    {
+        yield break;
+    }
+    public virtual IEnumerator Holding()
+    {
+        yield break;
+    }
+    public virtual IEnumerator Empty()
+    {
+        yield break;
+    }
+    public virtual IEnumerator Pointing()
+    {
+        yield break;
+    }
+
+    public virtual IEnumerator Fist()
+    {
+        yield break;
+    }
+
+    public virtual IEnumerator Shoot()
+    {
+        yield break;
+    }
+}
